934c62a80512930bf84354a3f208dcbd4f452601
Eduardo Aihara <eaihara90@gmail.com>
2020-10-29 22:18:48 -0300

    user-page posts

--- src/app/modules/user-page/components/user-posts/user-posts.component.ts
+++ src/app/modules/user-page/components/user-posts/user-posts.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-user-posts',
     templateUrl: './user-posts.component.html',
     styleUrls: ['./user-posts.component.scss']
 })
-export class UserPostsComponent {
+export class UserPostsComponent implements OnInit {
     
+    private posts: string[] = [
+        'Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5', 'Post 6', 'Post 7', 'Post 8'
+    ];
+
+    public rows: any[] = [];
+
+    ngOnInit(): void {
+        const rows = Math.round(this.posts.length / 3);
+
+        for (let i = 0; i < rows; i++) {
+            this.rows[i] = this.posts.splice(0, 3);
+        }
+        
+        console.log(this.rows)
+    }
 }
--- src/app/modules/user-page/user-page.module.ts
+++ src/app/modules/user-page/user-page.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { UserPageRoutes } from './user-page.routing';
 import { UserPageComponent } from './page/user-page.component';
 import { UserPostsComponent } from './components/user-posts/user-posts.component';
-import { UserPageRoutes } from './user-page.routing';
 import { UserTaggedComponent } from './components/user-tagged/user-tagged.component';
+import { PostComponent } from './components/user-posts/post/post.component';
 
 @NgModule({
     declarations: [
         UserPageComponent,
         UserPostsComponent,
-        UserTaggedComponent
+        UserTaggedComponent,
+        PostComponent
     ],
     imports: [
         CommonModule,
